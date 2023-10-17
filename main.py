554ec9a8227d0349f525cb66d2b512a34423d3ec
Slimcent <centpresley@gmail.com>
2023-10-17 20:37:04 +0100

    final algorithm

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer  # Replace CountVectorizer with TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Load the data from CSV
+data = pd.read_csv('spam_email_dataset.csv')
 
+# Check the first few rows of the dataset
+print(data.head())
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Define the feature (X) and target (y)
+X = data['Email']
+y = data['Spam Indicator']
 
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Create a TfidfVectorizer to convert text data to numerical features
+vectorizer = TfidfVectorizer()  # Replace CountVectorizer with TfidfVectorizer
+X_train = vectorizer.fit_transform(X_train)
+X_test = vectorizer.transform(X_test)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Initialize and train a Naive Bayes classifier
+nb_classifier = MultinomialNB()
+nb_classifier.fit(X_train, y_train)
+
+# Predict the labels for the test set
+y_pred = nb_classifier.predict(X_test)
+
+# Calculate the accuracy of the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy * 100:.2f}%')
+
+# Generate a confusion matrix and classification report
+confusion = confusion_matrix(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+print("Confusion Matrix:")
+print(confusion)
+print("Classification Report:")
+print(report)
